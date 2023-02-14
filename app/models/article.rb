6b6a09e1a43f89f3f34f461fc062e10fe120055a
Yazid Al Ghozali <yazid.al2418@gmail.com>
2023-02-15 00:06:58 +0700

    comment and commenter

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,13 @@
 class Article < ApplicationRecord
     has_many :comments
     
+    VALID_STATUS = ['public', 'private', 'archived']
+
+    validates :status, inclusion: { in: VALID_STATUS }
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
+
+    def archived?
+        status == 'archived'
+    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  VALID_STATUS = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUS }
+  
+  def archived? 
+    status == 'archived'
+  end
 end
