941240bafbb1b8016c1b5d5bc3a99adb83e2e889
Yazid Al Ghozali <yazid.al2418@gmail.com>
2023-02-16 00:59:52 +0700

    adding status and concern

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,13 +1,7 @@
 class Article < ApplicationRecord
+    include Visible
     has_many :comments
-    
-    VALID_STATUS = ['public', 'private', 'archived']
 
-    validates :status, inclusion: { in: VALID_STATUS }
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
-
-    def archived?
-        status == 'archived'
-    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,5 @@
 class Comment < ApplicationRecord
+  include Visible
+
   belongs_to :article
-
-  VALID_STATUS = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUS }
-  
-  def archived? 
-    status == 'archived'
-  end
 end
